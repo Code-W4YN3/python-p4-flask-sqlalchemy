c8357c4aee08ccef57408d99382b123084441d97
wayne-otido <otidowayne@gmail.com>
2023-09-19 14:37:53 +0300

    done

---
+++ server/migrations/versions/28d5f64d5ff2_.py
@@ -0,0 +1,24 @@
+"""Create tables owners, pets
+
+Revision ID: 28d5f64d5ff2
+Revises: 
+Create Date: 2023-09-19 14:33:39.501815
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '28d5f64d5ff2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
